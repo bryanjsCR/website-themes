cc4ca64ae4496fb581e83da58156308865cf08ef
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-05-16 14:50:03 +0300

    [MOVE]l10n_ua: keep original module here this module was renamed to l10n_ua_ifrs we keep original module here for databases which already have installed l10n_ua and are depended on it

---
+++ l10n_ua/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': u"Ukraine - Accounting IFRS",
+    'author': "ERP Ukraine",
+    'website': "https://erp.co.ua",
+    'summary': u"Український бухоблік згідно МСФЗ",
+    'version': '1.1',
+    'description': u"""
+Бухгалтерський облік для України (МСФЗ)
+=======================================
+
+Цей модуль дає можливість вести бухгалтерський
+облік діяльності підприємства згідно міжнародних
+стандартів фінансової звітності.
+
+Встановлюється типовий план рахунків,
+який підійде більшості підприємств.
+Також буде створено налаштування податків
+для обліку ПДВ
+    """,
+    'category': 'Localization/Account Charts',
+    'version': '1.1',
+    'depends': ['account'],
+    'data': [
+        'partner_view.xml',
+        'data/account_chart_template.xml',
+        'data/account.account.template.csv',
+        'data/account_tax_template.xml',
+        'data/account_chart_template_config.xml',
+        'data/account_chart_template.yml',
+    ],
+    'installable': True,
+}
