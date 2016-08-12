7b67f8b8ee42d2af1845f9cfdaf69dfb84dc84dd
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-08-12 20:53:26 +0300

    [imp] fb_instant_article

--- facebook_instant_article/controllers/main.py
+++ facebook_instant_article/controllers/main.py
@@ -2,10 +2,15 @@
 
 import werkzeug
 from openerp.addons.web import http
+from openerp import SUPERUSER_ID
 import requests
 from openerp.addons.web.http import request
+from openerp.addons.website.models.website import slug, unslug
 
 import pprint
+import lxml.html as LH
+from lxml import etree
+import json
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -44,7 +49,6 @@ class FbInstantArticle(http.Controller):
             r = requests.Session().get(FB_OAUTH_URL, params=par)
             if not r.status_code == 200:
                 response = r.json()
-                pprint.pprint(response)
                 return request.redirect(request.httprequest.url_root)
 
             response = r.json()
@@ -58,7 +62,6 @@ class FbInstantArticle(http.Controller):
             r = requests.Session().get(FB_OAUTH_URL, params=par)
             if not r.status_code == 200:
                 response = r.json()
-                pprint.pprint(response)
                 return request.redirect(request.httprequest.url_root)
 
             response = r.json()
@@ -74,6 +77,45 @@ class FbInstantArticle(http.Controller):
 
         return 'error'
 
+    #     curl \
+    # -F 'access_token={access-token}' \
+    # -F 'html_source=<!doctype html>...' \
+    # -F 'published=true' \
+    # -F 'development_mode=false' \
+    # https://graph.facebook.com/{page-id}/instant_articles
+    def _post_article_to_fb(self, html_source):
+        '''Upload article to facebook.
+        returns import id'''
+
+        user_id = request.env['res.users'].search(
+            [('id', '=', request.uid)],
+            limit=1)
+        if len(user_id) <= 0:
+            return False
+
+        res = request.env['website'].search(
+            [('id', '=', request.website.id)],
+            limit=1)
+        if len(res) <= 0:
+            return False
+
+        par = {
+            'access_token': user_id.fb_long_term_token,
+            'html_source': html_source,
+            'published': False,
+            'development_mode': True,
+        }
+        fb_instant_url = 'https://graph.facebook.com/' +\
+            res.fb_app_id + '/instant_articles'
+        r = requests.Session().post(fb_instant_url, params=par)
+        if not r.status_code == 200:
+            print r.json()
+            return False
+
+        response = r.json()
+        print 'resp : %s' % response
+        return response.get('id', '')
+
     @http.route(
         ['/fb_instant_article/disconnect'],
         type='http', auth='user')
@@ -85,3 +127,74 @@ class FbInstantArticle(http.Controller):
         if len(user_id) > 0:
             user_id.fb_long_term_token = ''
         return http.redirect_with_hash(redirect_uri)
+
+    @http.route([
+     '''/fb_instant_article/<model("blog.blog"):blog>''' +
+     '''/post/<model("blog.post", "[('blog_id','=',blog[0])]"):blog_post>''',
+    ], type='http', auth="user", website=True)
+    def fb_post(self,
+                blog,
+                blog_post):
+        """ Convert blog post to Facebook Instant Article.
+        """
+        cr, uid, context = request.cr, request.uid, request.context
+        blog_post_obj = request.registry['blog.post']
+
+        if not blog_post.blog_id.id == blog.id:
+            return request.redirect("/")
+
+        # Find next Post
+        all_post_ids = blog_post_obj.search(
+            cr, SUPERUSER_ID, [('blog_id', '=', blog.id)], context=context)
+        # should always return at least the current post
+        current_blog_post_index = all_post_ids.index(blog_post.id)
+        next_post_id = \
+            all_post_ids[0 if current_blog_post_index ==
+                         len(all_post_ids) - 1
+                         else current_blog_post_index + 1]
+        next_post = next_post_id and blog_post_obj.browse(
+            cr, SUPERUSER_ID, next_post_id, context=context) or False
+
+        post_cover_prop = json.loads(blog_post.cover_properties)
+        next_post_cover_prop = \
+            json.loads(next_post.cover_properties) if next_post else {}
+        values = {
+            'blog': blog,
+            'blog_post': blog_post,
+            'blog_post_cover_properties': post_cover_prop,
+            'main_object': blog_post,
+            'next_post': next_post,
+            'next_post_cover_properties': next_post_cover_prop,
+        }
+        post_id = blog_post_obj.browse(
+            cr, SUPERUSER_ID, blog_post.id, context=context)
+
+        layout = '<div>' + post_id.content + '</div>'
+        try:
+            root = LH.fromstring(layout)
+        except LH.ParseError as err:
+            _logger.warning('Can not parse article: %s' % format(err))
+            return False
+        for element in root.iter('h3'):
+            element.tag = 'h2'
+        for element in root.iter('h4'):
+            element.tag = 'h2'
+        for element in root.iter('img'):
+            src = element.attrib.get('src', '')
+            element.tag = 'figure'
+            for key in element.attrib.keys():
+                element.attrib.pop(key)
+            etree.SubElement(element, 'img').set('src', src)
+
+        post_id.fb_content = LH.tostring(root,
+                                         encoding='utf-8',
+                                         method='xml')
+
+        response = request.website.render(
+            "facebook_instant_article.blog_post_instant_article", values)
+        response.flatten()
+        import_id = self._post_article_to_fb(response.data)
+        if import_id:
+            post_id.fb_import_id = import_id
+        url = '/blog/' + str(blog.id) + '/post/' + str(blog_post.id)
+        return http.redirect_with_hash(url)
--- facebook_instant_article/models/website_blog.py
+++ facebook_instant_article/models/website_blog.py
@@ -3,6 +3,9 @@
 # from openerp import api, fields, models, _
 
 from openerp.osv import osv, fields
+from openerp.addons.website.models.website import slug
+from openerp import SUPERUSER_ID
+import requests
 
 
 class FbBlogPost(osv.Model):
@@ -16,3 +19,27 @@ class FbBlogPost(osv.Model):
             user = self.pool['res.users'].browse(cr, uid, uid, context=context)
             if user and user.fb_long_term_token:
                 print 'fb connected'
+                base_url = self.pool['ir.config_parameter'].get_param(
+                    cr, SUPERUSER_ID, 'web.base.url')
+                for post in self.browse(cr, uid, ids, context=context):
+                    post_url = '%(base_url)s/blog/%(blog_slug)s/post/%(post_slug)s' % {
+                        'base_url': base_url,
+                        'blog_slug': slug(post.blog_id),
+                        'post_slug': slug(post),
+                    }
+                    fb_article_url = '%(base_url)s/fb_instant_article/%(blog_slug)s/post/%(post_slug)s' % {
+                        'base_url': base_url,
+                        'blog_slug': slug(post.blog_id),
+                        'post_slug': slug(post),
+                    }
+                    print fb_article_url
+                    r = requests.Session().get(fb_article_url)
+                    if not r.status_code == 200:
+                        return
+                    response = r.json()
+                    print r.raw
+
+    _columns = {
+        'fb_content': fields.html('FB Content', sanitize=False),
+        'fb_import_id': fields.char('FB Content'),
+    }
