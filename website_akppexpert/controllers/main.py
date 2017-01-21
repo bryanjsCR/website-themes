0d26ce3a454452a70653ed21c02cad336e515421
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2017-01-21 22:33:00 +0200

    [IMP] search logic use OR logic on search query

--- website_akppexpert/controllers/main.py
+++ website_akppexpert/controllers/main.py
@@ -1,21 +1,47 @@
 # -*- coding: utf-8 -*-
 
-
 from openerp.addons.website_sale.controllers.main import website_sale
+from openerp.http import request
 
 
 class WebsiteSaleAkpp(website_sale):
 
     def _get_search_domain(self, search, category, attrib_values):
-        domain = super(WebsiteSaleAkpp, self)._get_search_domain(
-            search,
-            category,
-            attrib_values)
+        domain = request.website.sale_product_domain()
         if search:
-            for src in search.split(" "):
-                pre = domain[:1]
-                post = domain[1:]
-                domain = pre + [
-                    '|',
-                    ('attribute_line_ids.value_ids.name', 'ilike', src)] + post
+            dom = []
+            criterias = [
+                'name',
+                'description',
+                'description_sale',
+                'product_variant_ids.default_code',
+                'attribute_line_ids.value_ids.name'
+            ]
+            for srch in search.split(" "):
+                for opt in criterias:
+                    if len(dom) == 0:
+                        dom = [(opt, 'ilike', srch)]
+                    else:
+                        dom = ['|'] + dom + [(opt, 'ilike', srch)]
+            domain = domain + dom
+
+        if category:
+            domain += [('public_categ_ids', 'child_of', int(category))]
+
+        if attrib_values:
+            attrib = None
+            ids = []
+            for value in attrib_values:
+                if not attrib:
+                    attrib = value[0]
+                    ids.append(value[1])
+                elif value[0] == attrib:
+                    ids.append(value[1])
+                else:
+                    domain += [('attribute_line_ids.value_ids', 'in', ids)]
+                    attrib = value[0]
+                    ids = [value[1]]
+            if attrib:
+                domain += [('attribute_line_ids.value_ids', 'in', ids)]
+
         return domain
